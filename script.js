d402ec6d7d26fcc3718c1eeaf1c0c061072c4a23
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-10 14:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const thumbnails = document.querySelectorAll('.thumbnail');
+const fullImageContainer = document.querySelector('.full-image-container');
+const fullImage = document.getElementById('fullsize');
+const closeButton = document.getElementById('close-button');
+
+let isExpanded = false;
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', () => {
+        if (!isExpanded) {
+            const thumbnailSrc = thumbnail.getAttribute('src');
+            fullImage.setAttribute('src', thumbnailSrc);
+            fullImageContainer.style.display = 'flex';
+            isExpanded = true;
+        }
+    });
+});
+
+closeButton.addEventListener('click', () => {
+    fullImageContainer.style.display = 'none';
+    isExpanded = false;
+});
+
